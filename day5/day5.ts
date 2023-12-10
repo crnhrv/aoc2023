083f102b78213c27ab43642452acad2cb691ee3e
Ciaran Harvey <ciaran.harvey@red-gate.com>
2023-12-10 20:26:37 +0000

    p2 bruteforce solution

--- day5/day5.ts
+++ day5/day5.ts
@@ -11,6 +11,11 @@ type Mapping = {
     length: number;
 }
 
+type SourceValue = {
+    start: number;
+    end: number;
+}
+
 ((inputFile: string) => {
     const input = fs.readFileSync(inputFile, "utf-8").split("\n").filter(x => x.length > 1);
     var sourceValues = input[0].split(":")[1].trim().split(" ").map(Number);
@@ -47,21 +52,47 @@ type Mapping = {
         
     }
 
-    for (const map of maps) {
-        const newSourceVals : number[] = []
-        for (const sourceVal of sourceValues) {
-            var destVal = sourceVal;
-            const mapping = map.mappings.find(x => (x.source + x.length - 1) >= sourceVal && x.source <= sourceVal);
-            if (mapping !== undefined) {
-                destVal = (sourceVal - mapping.source) + mapping.destination
-            }
+    const minp1 = getMinLocation(maps, calculateSourceValues([...sourceValues], false));
+    console.log(minp1);   
 
-            newSourceVals.push(destVal)            
-        }
-        sourceValues = newSourceVals;
+    const minp2 = getMinLocation(maps, calculateSourceValues([...sourceValues], true));
+    console.log(minp2);
+})("./input.txt")
+
+
+function calculateSourceValues(originalValues: number[], isRange: boolean) : SourceValue[] {
+    const sourceValues : SourceValue[] = [];
+
+    for (let i = 0; i < originalValues.length; isRange ? i += 2 : i++) {
+        const seedStart = originalValues[i];
+        const seedEnd = isRange ? seedStart + (originalValues[i + 1] - 1) : seedStart;
+
+        const value: SourceValue = {            
+            start: seedStart,
+            end: seedEnd,
+        };
+
+        sourceValues.push(value);
     }
 
-    console.log(Math.min(...sourceValues))
-    
-})("./test-input.txt")
+    return sourceValues;
+}
+
+
+function getMinLocation(maps: Map[], sourceValues: SourceValue[]) : number {
+    var minLocation = Infinity;
+    for (const sourceVal of sourceValues) {
+        for (let i = sourceVal.start; i <= sourceVal.end; i++) {
+            let destinationValue = i;
+            for (const map of maps) {            
+                const mapping = map.mappings.find(x => (x.source + x.length - 1) >= destinationValue && x.source <= destinationValue);
+                if (mapping !== undefined) {
+                    destinationValue = (destinationValue - mapping.source) + mapping.destination;                
+                }  
+            }
+            minLocation = Math.min(destinationValue, minLocation)
+        }
+    }
+    return minLocation;
+}
 
